93617def04fbfb54d944280b3e1f6a93c861cfad
ivanslavov228 <slavov894@gmail.com>
2025-11-25 14:18:36 +0300

    Update main.py

--- 2.3lab/main.py
+++ 2.3lab/main.py
@@ -1,2 +1,11 @@
-if __name__ == "__main__":
-    pass
+def SumMtr(matrix1, matrix2):
+    if len(matrix1) != len(matrix2) or len(matrix1[0]) != len(matrix2[0]):
+        raise ValueError("Матрицы должны быть одного размера")
+    
+    result = []
+    for i in range(len(matrix1)):
+        row = []
+        for j in range(len(matrix1[0])):
+            row.append(matrix1[i][j] + matrix2[i][j])
+        result.append(row)
+    return result
