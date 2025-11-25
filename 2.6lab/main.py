44c224dccd958fbb50e3b0a848bdfb2a80f91fc0
ivanslavov228 <slavov894@gmail.com>
2025-11-25 14:21:09 +0300

    Update main.py

--- 2.6lab/main.py
+++ 2.6lab/main.py
@@ -1,2 +1,14 @@
-if __name__ == "__main__":
-    pass
+def remove_char(text, char):
+    return text.replace(char, '')
+
+def replace_char(text, old_char, new_char):
+    return text.replace(old_char, new_char)
+
+def count_char(text, char):
+    return text.count(char)
+
+# Пример использования
+text = "hello world"
+print(remove_char(text, 'l'))
+print(replace_char(text, 'o', '0'))
+print(count_char(text, 'l'))
