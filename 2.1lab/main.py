1635eea5e06f73b8f6fd73d5268f83ca445ee08c
ivanslavov228 <slavov894@gmail.com>
2025-11-25 14:17:46 +0300

    Update main.py

--- 2.1lab/main.py
+++ 2.1lab/main.py
@@ -1,2 +1,14 @@
-if __name__ == "__main__":
-    pass
+# а)
+n = int(input())
+result_dict = {i: i*i for i in range(1, n+1)}
+print(result_dict)
+
+# б)
+dicts_list = eval(input())
+result_list = [list(d.values())[0] for d in dicts_list]
+print(result_list)
+
+# в)
+input_dict = eval(input())
+result_dict = {k: v for k, v in input_dict.items() if v not in [None, '']}
+print(result_dict)
