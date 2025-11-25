bbd92e15aadc24d8bb78daf11caacd404622ed78
ivanslavov228 <slavov894@gmail.com>
2025-11-25 14:17:20 +0300

    Update main.py

--- 2.10lab/main.py
+++ 2.10lab/main.py
@@ -1,2 +1,24 @@
-if __name__ == "__main__":
-    pass
+# а)
+try:
+    num = float(input())
+    power = int(input())
+    if power < 0:
+        raise ValueError("Отрицательная степень")
+    print(num ** power)
+except ValueError as e:
+    print(e)
+
+# б)
+try:
+    num = int(input())
+    if num % 2 != 0:
+        raise ValueError("Число не является четным")
+    print("Число является четным")
+except ValueError as e:
+    print(e)
+
+# в)
+num = float(input())
+power = int(input())
+assert power >= 0, "Отрицательная степень"
+print(num ** power)
