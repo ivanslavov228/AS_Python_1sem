973eb3271f8b274ca3e68ea9f91cc4723b83c0ed
ivanslavov228 <slavov894@gmail.com>
2025-11-25 14:20:16 +0300

    Update main.py

--- 2.5lab/main.py
+++ 2.5lab/main.py
@@ -1,2 +1,7 @@
-if __name__ == "__main__":
-    pass
+def NOD(A, B):
+    if A == 0:
+        return B
+    return NOD(B % A, A)
+
+A, B, C, D = map(int, input().split())
+print(NOD(A, B), NOD(A, C), NOD(A, D))
