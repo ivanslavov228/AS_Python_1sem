0569dee39754fd5636c84597b03e2806b40f54cc
ivanslavov228 <slavov894@gmail.com>
2025-11-25 14:19:43 +0300

    Update main.py

--- 2.4lab/main.py
+++ 2.4lab/main.py
@@ -1,2 +1,24 @@
-if __name__ == "__main__":
-    pass
+# а)
+def word_count(text, separator=' '):
+    words = text.split(separator)
+    count_dict = {}
+    for word in words:
+        count_dict[word] = count_dict.get(word, 0) + 1
+    return count_dict
+
+# б)
+def filter_numbers(numbers, exclude_multiples_of=2):
+    return [num for num in numbers if num % exclude_multiples_of != 0]
+
+# в)
+def merge_and_sort_keys(*dicts):
+    merged = {}
+    for d in dicts:
+        for k, v in d.items():
+            if k not in merged or v > merged[k]:
+                merged[k] = v
+    return sorted(merged.keys(), key=lambda x: merged[x], reverse=True)
+
+# г)
+def sum_non_negative(*args):
+    return sum(num for num in args if num >= 0)
