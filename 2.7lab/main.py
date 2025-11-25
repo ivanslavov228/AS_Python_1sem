3bdecb297145157ae895c759fc3af0ff279ca190
ivanslavov228 <slavov894@gmail.com>
2025-11-25 14:21:41 +0300

    Update main.py

--- 2.7lab/main.py
+++ 2.7lab/main.py
@@ -1,2 +1,7 @@
-if __name__ == "__main__":
-    pass
+with open('input.txt', 'r', encoding='utf-8') as f_in:
+    text = f_in.read()
+
+punctuation = [char for char in text if char in '.,!?;:—-"\'()[]{}']
+
+with open('output.txt', 'w', encoding='utf-8') as f_out:
+    f_out.write(''.join(punctuation))
