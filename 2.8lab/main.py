0cc82912d42657a9a006c46fc23ddcdd09806498
ivanslavov228 <slavov894@gmail.com>
2025-12-02 12:10:18 +0300

    Update main.py

--- 2.8lab/main.py
+++ 2.8lab/main.py
@@ -1,2 +1,4 @@
-if __name__ == "__main__":
-    pass
+with open('f.txt', 'r') as file:
+    numbers = map(int, file.read().split())
+    count = sum(1 for num in numbers if num % 2 == 0)
+    print(count)
