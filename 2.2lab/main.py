9ea11092d94da01c0c5d114f30cb6672b0267b78
ivanslavov228 <slavov894@gmail.com>
2025-12-02 12:09:47 +0300

    Update main.py

--- 2.2lab/main.py
+++ 2.2lab/main.py
@@ -1,14 +1,4 @@
-# а)
-n = int(input())
-result_dict = {i: i*i for i in range(1, n+1)}
-print(result_dict)
-
-# б)
-dicts_list = eval(input())
-result_list = [list(d.values())[0] for d in dicts_list]
-print(result_list)
-
-# в)
-input_dict = eval(input())
-result_dict = {k: v for k, v in input_dict.items() if v not in [None, '']}
-print(result_dict)
+def TriangleP(a, h):
+    b = ((a / 2) ** 2 + h ** 2) ** 0.5
+    perimeter = a + 2 * b
+    return perimeter
